f73b5fc8a1847d98f783efc593f4bdf7a4c6d29b
IvanPolienko <piar1986@yandex.ru>
2020-04-13 22:36:21 +0700

    Added script to format and upload photos to instagram

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,3 @@
 from .fetch_hubble import *
 from .fetch_spacex import *
+from .upload_photos import *
---
+++ scripts/upload_photos.py
@@ -0,0 +1,40 @@
+import os
+from urllib.parse import urljoin
+from PIL import Image
+from os import listdir
+from instabot import Bot
+from dotenv import load_dotenv
+
+
+def format_images(source_folder='images', recipient_folder='formated_images'):
+    files = listdir(source_folder)
+    for file in files:
+        image_path = os.path.join(source_folder, file)
+        image = Image.open(image_path)
+        image_trim_size = abs(image.height - image.width)/2
+
+        if image.width < image.height:
+            image_new_coordinates = (0, image_trim_size, image.width, image.height - image_trim_size)
+        else:
+            image_new_coordinates = (image_trim_size, 0, image.width - image_trim_size, image.height)
+
+        cropped_image = image.crop(image_new_coordinates)
+        cropped_image.thumbnail((1080, 1080))
+
+        image_title = file.split('.')[0]
+        new_image = image_title + '.jpg'
+        new_image_path = os.path.join(recipient_folder, new_image)
+        cropped_image.save(new_image_path)
+
+def upload_images(source_folder='formated_images'):
+    load_dotenv()
+    login = os.getenv("INSTAGRAM_LOGIN")
+    password = os.getenv("INSTAGRAM_PASSWORD")
+
+    bot = Bot()
+    bot.login(username=login, password=password, proxy=None)
+
+    files = listdir(source_folder)
+    for file in files:
+        image_path = os.path.join(source_folder, file)
+        bot.upload_photo(image_path, caption="Space is great!")
